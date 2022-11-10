cc21546ce989152ebea50bb5b29df8bd8d8f024c
Tucker Dickson <nathan-dickson@uiowa.edu>
2022-11-10 09:53:28 -0600

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ctrl+alt+elite`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
