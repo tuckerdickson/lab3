0214518d6b58c025d4371bb8256fd545335d36db
ryan-edwall <111799742+ryan-edwall@users.noreply.github.com>
2022-11-15 02:32:07 -0600

    member showcase example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,161 +1,69 @@
 import * as React from "react"
 import { render } from "react-dom";
 import '../fonts/stylesheet.css';
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import pic from  '../images/default_avatar.png'
 
 const pageheader =
 {
   fontFamily: "UbuntuMono",
 }
 
+const Abigail =
+{
+  name:"Abigail Bright",
+  major:"Electrical Engineering: Computer Track",
+  imgPath: pic
+}
+
+const Tucker =
+{
+  name: "Tucker Dickson",
+  major:"Computer Science and Engineering",
+  imgPath: pic
+}
+
+const Cathryn =
+{
+  name: "Cathryn Lyons",
+  major:"Computer Science and Engineering",
+  imgPath: pic
+}
+
+const Ryan =
+{
+  name: "Ryan Edwall",
+  major: "Electrical Engineering: Computer Track",
+  imgPath: pic
+}
+
+const groupMembers = [Abigail,Tucker,Cathryn,Ryan]
+
+
 function MemberList(members)
 {
   return(
     <div>
       {
-        members.names.map(members =>
-          <button>{members}</button>
+        members.groupMembers.map(memberInfo =>
+          <th>
+            <img src = {memberInfo.imgPath} width = "50%" height= "50%"></img>
+            <p>{memberInfo.name}</p>
+            <p>{memberInfo.major}</p>
+          </th>
           )
       }
     </div>
   )
 }
-
-const groupMembers = ["Abigail","Tucker","Cathryn","Ryan"]
-const element = <MemberList names = {groupMembers}/>
-
+const displayMembers = <MemberList groupMembers = {groupMembers}/>
 const IndexPage = () => {
   return (
     <main>
-
     <h1 style = {pageheader}> Meet Control Alt Elite!</h1>
-    {element}
+    {displayMembers}
     </main>
   )
 }
-
 export default IndexPage
 
 export const Head = () => <title>Home Page</title>
