71e77403661c27d21c5453b51dfb002964e3af9c
Abigail Bright <abigail-bright@uiowa.edu>
2022-11-22 14:30:54 -0600

    starting abigail page

---
+++ src/pages/abigail.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const AbigailPage = () => {
+  return (
+    <main>
+    <h1> This is Abigail's Page!</h1>
+    </main>
+  )
+}
+export default AbigailPage
+
+export const Head = () => <title>Abigail's Page</title>
