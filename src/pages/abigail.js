38148ea24656d931bbba48eec9f8cdf1286fb3a3
Abigail Bright <abigail-bright@uiowa.edu>
2022-11-22 15:39:54 -0600

    made personal page

--- src/pages/abigail.js
+++ src/pages/abigail.js
@@ -2,9 +2,8 @@ import * as React from "react"
 
 //importing images
 import volleyball from '../abigail_images/volleyball.jpg'
-import kona2 from '../cathryn_images/KonaOnCouch.jpeg'
-import kona3 from '../cathryn_images/KonaOutside.jpeg'
-import kona4 from '../cathryn_images/KonaWithToy.jpeg'
+import calligraphy from '../abigail_images/calligraphy.jpg'
+import ferrari from '../abigail_images/ferrari.jpeg'
 import abigailPortrait from '../images/abigail_portrait.jpeg'
 
 const AbigailPage = () => {
@@ -21,20 +20,19 @@ const AbigailPage = () => {
     <p>Hi, I'm Abigail! I'm a senior majoring in Electrical and Computer Engineering at the University of Iowa.</p>
     <img src = {abigailPortrait} height = "25%" width = "25%"  alt = {abigailPortrait} class = "center"></img>
 
-    <h1>Something</h1>
-    <p>Get rid of Kona </p>
-    <p>Make myself seem cool:</p>
-    <p>&#x2022; Calligraphy</p>
+    <h1>Personal Interests</h1>
     <p>&#x2022; Volleyball</p>
-    <p>&#x2022; Being cute</p>
-    <img src = {volleyball} height = "25%" width = "25%" alt = {volleyball}></img>
-    <img src = {kona2} height = "25%" width = "25%" alt = {kona2}></img>
-    <img src = {kona3} height = "25%" width = "25%" alt = {kona3}></img>
-    <img src = {kona4} height = "25%" width = "25%" alt = {kona4}></img>
+    <p>&#x2022; Formula 1 (Forza Ferrari!)</p>
+    <p>&#x2022; Calligraphy</p>
+    <div id="pics">
+    <img src = {volleyball} height = "37%" width = "37%" alt = {volleyball}></img>
+    <img src = {ferrari} height = "25%" width = "25%" alt = {ferrari}></img>
+    <img src = {calligraphy} height = "38%" width = "38%" alt = {calligraphy}></img>
+    </div>
+   
 
     <h1>Fun Facts</h1>
-    <p>&#x2022; I love Formula 1 (Forza Ferrari!)</p>
-    <p>&#x2022; I come from a long line of Hawkeyes</p>
+    <p>&#x2022; My go-to karaoke song is <a href="https://youtu.be/Vy0mqXuBMY4">I Write Sins Not Tragedies</a> by Panic! At The Disco</p>
     <p>&#x2022; I broke both of my wrists at the same time and had double hot pink casts</p>
     <p>&#x2022; My favorite animal is a stingray</p>
     </div>
