243724b766b6fb124203e3aa1f380e2f617a9308
Abigail Bright <abigail-bright@uiowa.edu>
2022-11-22 14:58:58 -0600

    started an abigail js page

--- src/pages/abigail.js
+++ src/pages/abigail.js
@@ -1,9 +1,43 @@
 import * as React from "react"
 
+//importing images
+import kona1 from '../cathryn_images/KonaInCoat.jpeg'
+import kona2 from '../cathryn_images/KonaOnCouch.jpeg'
+import kona3 from '../cathryn_images/KonaOutside.jpeg'
+import kona4 from '../cathryn_images/KonaWithToy.jpeg'
+import abigailPortrait from '../images/abigail_portrait.jpeg'
+
 const AbigailPage = () => {
   return (
     <main>
-    <h1> This is Abigail's Page!</h1>
+    <div id = "background">
+
+    <a href="/">Back to Home Page</a>  
+    <p></p>
+    <div id="indexheader">
+    <h1>About Abigail</h1>
+    </div>
+    
+    <p>Hi, I'm Abigail! I'm a senior majoring in Electrical and Computer Engineering at the University of Iowa.</p>
+    <img src = {abigailPortrait} height = "25%" width = "25%"  alt = {abigailPortrait} class = "center"></img>
+
+    <h1>Something</h1>
+    <p>Get rid of Kona </p>
+    <p>Make myself seem cool:</p>
+    <p>&#x2022; Calligraphy</p>
+    <p>&#x2022; Volleyball</p>
+    <p>&#x2022; Being cute</p>
+    <img src = {kona1} height = "25%" width = "25%" alt = {kona1}></img>
+    <img src = {kona2} height = "25%" width = "25%" alt = {kona2}></img>
+    <img src = {kona3} height = "25%" width = "25%" alt = {kona3}></img>
+    <img src = {kona4} height = "25%" width = "25%" alt = {kona4}></img>
+
+    <h1>Fun Facts</h1>
+    <p>&#x2022; I love Formula 1 (Forza Ferrari!)</p>
+    <p>&#x2022; I come from a long line of Hawkeyes</p>
+    <p>&#x2022; I broke both of my wrists at the same time and had double hot pink casts</p>
+    <p>&#x2022; My favorite animal is a stingray</p>
+    </div>
     </main>
   )
 }
