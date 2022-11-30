cfd8b995afc4a0b83f5451d47b2c43a3b832721d
Cathryn Lyons <cathryn-lyons@uiowa.edu>
2022-11-29 19:46:09 -0600

    comment box on ryan page

--- src/pages/ryan.js
+++ src/pages/ryan.js
@@ -1,9 +1,32 @@
 import * as React from "react"
 
+import commentBox from 'commentbox.io';
+
+class PageWithComments extends React.Component {
+
+    componentDidMount() {
+
+        this.removeCommentBox = commentBox('5680389167251456-proj');
+    }
+
+    componentWillUnmount() {
+
+        this.removeCommentBox();
+    }
+
+    render() {
+
+        return (
+            <div className="commentbox" />
+        );
+    }
+}
+
 const RyanPage = () => {
   return (
     <main>
     <h1> This is Ryan's Page!</h1>
+    <PageWithComments />
     </main>
   )
 }
